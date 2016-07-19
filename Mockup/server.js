ca1d364538ef27315947e7f2f5f3c814642366bb
Timpan5 <tim.pan@mail.utoronto.ca>
2016-07-19 15:28:42 -0400

    Basic server.js

---
+++ Mockup/server.js
@@ -0,0 +1,63 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+
+const PORT=8888;
+
+//Create server
+var server = http.createServer( function (request, response) {  
+   var pathname = url.parse(request.url).pathname;
+   
+   //Check requested resource
+   console.log("Request: " + pathname);
+   
+   //Root directory
+   if (pathname.substr(1) == '') {
+		fs.readFile("index.html", function (err, data) {
+		sendData({'Content-Type': 'text/html'}, data, response);
+		console.log('Sent index.html');
+		});
+	}
+	
+	//Send css 
+	else if (pathname.substr(1) == 'assets/css/style.css') {
+		fs.readFile("assets/css/style.css", function (err, data) {
+		sendData({'Content-Type': 'text/css'}, data, response);
+		console.log('Sent css');
+		});
+	}
+	
+	//Send front-end js
+	else if (pathname.substr(1) == 'assets/scripts/script.js') {
+		fs.readFile("assets/scripts/script.js", function (err, data) {
+		sendData({'Content-Type': 'text/javascript'}, data, response);
+		console.log('Sent js');
+		});
+	}
+	
+	//Send jQuery library
+	else if (pathname.substr(1) == 'assets/scripts/jquery-2.2.4.min.js') {
+		fs.readFile("assets/scripts/jquery-2.2.4.min.js", function (err, data) {
+		sendData({'Content-Type': 'text/javascript'}, data, response);
+		console.log('Sent jquery');
+		});
+	}
+	
+	//Url not recognized
+	else {	
+        response.writeHead(404, {'Content-Type': 'text/html'});	
+		response.end();
+		console.log('Unknown request');
+	}  
+});
+
+server.listen(PORT);
+console.log('Server running at http://127.0.0.1:8888/');
+
+//Send response back
+function sendData(textHead, data, response) {
+	    response.writeHead(200, {textHead});	
+		response.write(data);
+		response.end();
+}
